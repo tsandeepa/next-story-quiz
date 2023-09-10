a5da00234ecd3c98f4d7fddd96378d07750e253a
tharindu <mustythemist@gmail.com>
2023-09-11 02:29:21 +0530

    sound efects added

--- pages/qz/[slug].js
+++ pages/qz/[slug].js
@@ -55,6 +55,58 @@ const QuestionView = () => {
 
   const bt_inc = useRef();
   const bt_dec = useRef();
+  const audioRef = useRef(null);
+  const audio_locked = useRef(null);
+  const audio_wrong = useRef(null);
+  const audio_correct = useRef(null);
+  const audio_letsplay = useRef(null);
+  const audio_thinking = useRef(null);
+
+  useEffect(() => {
+
+    const playLetsPlay = () => {
+      if (audio_letsplay.current) {
+        audio_letsplay.current.play();
+      }
+    };
+
+    const playThinking = () => {
+      if (audio_thinking.current) {
+        audio_thinking.current.loop = true;
+        audio_thinking.current.play();
+      }
+    };
+
+    setTimeout(() => {
+      playLetsPlay();
+    }, 500);
+
+    setTimeout(() => {
+      playThinking()
+    }, 6000);
+
+
+    return () => {
+      if (audio_thinking.current) {
+        audio_thinking.current.pause();
+        audio_thinking.current.currentTime = 0;
+      }
+    };
+
+  }, [qzNo]);
+
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+      setTimeout(() => {
+        const lockedAudio = audio_locked.current;
+        lockedAudio.loop = true; // Set the loop attribute to true
+        lockedAudio.play();
+      }, 3000);
+    }
+  };
+
 
   useEffect(() => {
 
@@ -86,12 +138,17 @@ const QuestionView = () => {
 
   const removeActiveClass = () => {
     const slected = qz_opt.current.querySelectorAll('.active');
+    const slectedWrong = qz_opt.current.querySelectorAll('.active-wrong');
+
+    console.log('slectedWrong', slectedWrong);
+
     if (slected.length == 1) {
-      slected[0].classList.remove('active')
+      slected[0].classList.remove('active');
     }
     const coorectClass = qz_opt.current.querySelectorAll('.correct');
     if (coorectClass.length == 1) {
       coorectClass[0].classList.remove('correct');
+      slectedWrong[0]?.classList.remove('active-wrong');
     }
 
     document.querySelector('.main-view').classList.remove('answer-won')
@@ -100,6 +157,9 @@ const QuestionView = () => {
   }
 
   const selectAnswer = (e, answer) => {
+
+    audio_thinking.current.pause();
+    audio_thinking.current.currentTime = 0;
 
     const slected = qz_opt.current.querySelectorAll('.active');
 
@@ -122,17 +182,26 @@ const QuestionView = () => {
   }
 
   const revealAnswer = (answer) => {
+
+    audio_thinking.current.pause();
+    audio_thinking.current.currentTime = 0;
+
     console.log(selectedAnswer, answer);
     let correctAnswer = qz_opt.current.querySelector(`#${answer}`);
     correctAnswer.classList.add('correct')
 
-    const revealEffect = document.querySelector('.main-view')
+    const revealEffect = document.querySelector('.main-view');
+    const slected = qz_opt.current.querySelector('.active');
 
     if (selectedAnswer) {
       revealEffect.classList.add('answer-won');
+      audio_wrong.current.play();
+      audio_locked.current.pause();
     } else {
       revealEffect.classList.add('answer-defeated');
-
+      audio_correct.current.play();
+      audio_locked.current.pause();
+      slected.classList.add('active-wrong');
     }
   }
 
@@ -152,13 +221,21 @@ const QuestionView = () => {
           {
             <div>
               <h3 className="text-3xl text-white my-3">{data.story.content.qz_blocks[`${qzNo}`].question_title}</h3>
+              <div onClick={playAudio}>
+                <ul ref={qz_opt} className="grid grid-cols-2 gap-4 mt-8" onClick={(e) => selectAnswer(e, data.story.content.qz_blocks[`${qzNo}`].answer)}>
+                  <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="a">A - {data.story.content.qz_blocks[`${qzNo}`].a}</li>
+                  <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="b">B -{data.story.content.qz_blocks[`${qzNo}`].b}</li>
+                  <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="c">C -{data.story.content.qz_blocks[`${qzNo}`].c}</li>
+                  <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="d">D -{data.story.content.qz_blocks[`${qzNo}`].d}</li>
+                </ul>
+              </div>
 
-              <ul ref={qz_opt} className="grid grid-cols-2 gap-4 mt-8" onClick={(e) => selectAnswer(e, data.story.content.qz_blocks[`${qzNo}`].answer)}>
-                <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="a">A - {data.story.content.qz_blocks[`${qzNo}`].a}</li>
-                <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="b">B -{data.story.content.qz_blocks[`${qzNo}`].b}</li>
-                <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="c">C -{data.story.content.qz_blocks[`${qzNo}`].c}</li>
-                <li className="bg-slate-500 rounded-sm p-5 hover:bg-opacity-80 cursor-pointer" id="d">D -{data.story.content.qz_blocks[`${qzNo}`].d}</li>
-              </ul>
+              <audio ref={audioRef} src='/audio/confirm.mp3' />
+              <audio ref={audio_locked} src='/audio/confirm_loop.mp3' />
+              <audio ref={audio_wrong} src='/audio/correct.mp3' />
+              <audio ref={audio_correct} src='/audio/wrong.mp3' />
+              <audio ref={audio_letsplay} src='/audio/lets_play.mp3' />
+              <audio ref={audio_thinking} src='/audio/Thinkinng.mp3' />
               <div className="mt-6  bg-black">
                 <img className="max-h-60 mx-auto" src={data.story.content.qz_blocks[`${qzNo}`].question_img[0]?.filename} alt="" />
               </div>
