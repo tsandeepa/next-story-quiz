f896857095bbb1d2a0ac787c0c4416964a8848f7
tharindu <mustythemist@gmail.com>
2023-09-17 01:14:33 +0530

    api question  page issue fixed

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -16,24 +16,24 @@ export default Questions;
 
 
 
-export async function getStaticProps() {
-  // home is the default slug for the homepage in Storyblok
-  let slug = "qz/grade-8";
+// export async function getStaticProps() {
+//   // home is the default slug for the homepage in Storyblok
+//   let slug = "qz/grade-8";
 
-  // load the draft version
-  let sbParams = {
-    version: "draft", // or 'published'
-    // starts_with: "qz/",
+//   // load the draft version
+//   let sbParams = {
+//     version: "draft", // or 'published'
+//     // starts_with: "qz/",
 
-  };
+//   };
 
-  const storyblokApi = getStoryblokApi();
-  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+//   const storyblokApi = getStoryblokApi();
+//   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
 
-  return {
-    props: {
-      data
-    },
-    revalidate: 3600, // revalidate every hour
-  };
-}
+//   return {
+//     props: {
+//       data
+//     },
+//     revalidate: 3600, // revalidate every hour
+//   };
+// }
